9ddf457dbb95eb06da7aa6679089507e4faceac8
Osmar Cuicas <Osmargabriel10@gmail.com>
2025-05-09 18:45:27 -0400

    Work in Progress

---
+++ BlazorApp1/Components/Pages/RegisterUser/RegisterUser.razor.cs
@@ -0,0 +1,13 @@
+using BlazorApp1.Data.Auth;
+using Core.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace BlazorApp1.Components.RegisterUser
+{
+    public partial class RegisterUser
+    {
+        public User user = new User();
+
+    }
+}
